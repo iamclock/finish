97bab26e2a9328335360eebf5b504304d1536f99
clock <double7@mail.com>
2017-11-23 17:58:22 +0700

    lsfr and prak merging

---
+++ src/compile.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# скрипт запускает транслятор со старых символов на новые файл name_.l
+# затем компилирует выходной файл
+# и удаляет промежуточные файлы .s _.l
+
+
+
+
+if [ $# -eq 0 ]
+	then
+		echo -n "Enter file name by first argument: "
+		#var file_name
+		read file_name
+else
+	file_name=$1
+fi
+var=`basename $file_name .l`
+./for_compile/translator.py $file_name
+./for_compile/ltc $var"_.l"
+#if [ $ret -eq 0 ]
+#	then
+#		./for_compile/ltc $var"_.l"
+#		rm $var"_.s" $var"_.l"
+#else
+#	echo "translator.py exit with errors"
+#fi
+rm $var"_.s" $var"_.l"
+
